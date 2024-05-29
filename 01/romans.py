4daa81856f6f21fe0bde0f7a52651b68a570bb47
Felipe Tironi <168040604+fetironi@users.noreply.github.com>
2024-05-28 21:55:44 -0300

    eu fiz Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,5 +1,19 @@
 def int_to_roman(num):
-    # Implemente sua função aqui
+
+    num2 = num // 1000
+    num3 = (num % 1000)//100
+    num4 = (num % 100)//10
+    num5 = num % 10
+
+    milhar = ["","M","MM","MMM"]
+    centena = ["","C","CC","CCC","CD","D","DC","DCC", "DCCC","CM"]
+    dezena = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+    unidade = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+
+    traducao = milhar[num2] + centena[num3] + dezena[num4] + unidade[num5]
+
+    return(traducao)
+
     pass
 
 
